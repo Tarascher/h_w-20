c5317b0ac5b68809428ece90a46cecdaf5c992c1
Tarascher <117759750+Tarascher@users.noreply.github.com>
2022-12-23 11:57:18 +0200

    oll number

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,24 @@
 let size = +prompt(`введите число `);
 let n = size;
 
-let isFail = true;
+let isTrue = true;
 for (let i = 2; i < n; i++) {
   console.log(`${i}`);
   if (size == 0) {
     alert(`0`);
-    isFail = false;
+    isTrue = false;
     break;
-  } else if (size >= 101) {
+  } /*else if (size >= 101) {
     alert(`число с не деепазона`);
-    isFail = false;
+    isTrue = false;
     break;
-  } else if (n % i == 0) {
+  }*/ else if (n % i == 0) {
     alert(`ура число ${n} сложное`);
-    isFail = false;
+    isTrue = false;
     break;
   }
 }
-if (isFail) {
+if (isTrue) {
   alert(`Число ${n} простое крайнее число  делимое на 1 и  ${n}`);
 }
 /*let text = prompt(`введите текст`);
